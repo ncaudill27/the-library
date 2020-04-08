13f4521d6fa23bd427e35b43527bf5263ffa6d9b
theMindStone <nelson.a.caudill@gmail.com>
2020-04-08 03:32:16 -0400

    Confirm all controller are connected

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :boards
   namespace :api do
     namespace :v1 do
       resources :users
       resources :clubs    
+      resources :comments
+      resources :boards
     end
   end
 end
