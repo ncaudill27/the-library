2c2f710df44072e415c6303cc2abf0180ec2f4e0
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-08 22:45:43 -0400

    Create SidebarContainer

---
+++ client/src/containers/SideBarContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SidebarContainer extends Component {
+  render() {
+    return (
+      <div className='container-sidebar'>
+        <UserBox />
+        <ClubList clubs={} />
+      </div>
+    )
+  }
+}
