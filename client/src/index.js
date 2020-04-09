acc9283663ab21fe05a22893f70522093944fa3c
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-09 14:06:04 -0400

    Begin adding Router

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
+      <>
+        {/* <Route path='/' component={Home} /> */}
+        <Route exact path='/clubs' component={ClubList} />
+      </>
+    </Router>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
