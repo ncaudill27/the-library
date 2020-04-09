d78d5b87890166e9b1459de778033333f0aa31c5
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-08 22:36:48 -0400

    Create InputField component

---
+++ client/src/components/InputField.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const InputField = ({handleSubmit}) => (
+  <div className='input-field'>
+    <input type='text' onChange={} />
+    <button onClick={handleSubmit} >Click me!</button>
+  </div>
+)
+
+export default InputField;
