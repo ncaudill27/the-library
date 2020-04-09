f8d2891c359182f46886ede200371ff3c6106adf
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-08 23:03:15 -0400

    Create ClubList component

---
+++ client/src/components/ClubList.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ClubSibebar from './ClubSidebar';
+import ClubMain from './ClubMain';
+
+class ClubList extends Component {
+
+  renderClubs = () => {
+    return this.props.clubs.map( club => <ClubSibebar club={club} /> );
+  }
+
+  render() {
+    return (
+      <div className='list-clubs'>
+        <h2>Clubs</h2>
+        {this.renderClubs()}
+        <div className='find-club' onClick={}>
+          <h3>Find Club</h3>
+        </div>
+      </div>
+    )
+  }
+}
