1eddabc790ee5d2b0969ed26e39846239fe42316
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-08 22:51:15 -0400

    Create UserBox component

---
+++ client/src/components/UserBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Avatar from './Avatar';
+
+const UserBox = () => (
+  <div className='user-box'>
+    <Avatar username={} avatar={} />
+    <p>Username</p>
+    <p>Settings</p>
+  </div>
+)
