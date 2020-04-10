23b5705a0c29137949af0f3b4807e4de344b6fec
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-10 00:44:35 -0400

    Connect clubsReducer to MainContainer

--- client/src/reducers/clubsReducer.js
+++ client/src/reducers/clubsReducer.js
@@ -1,14 +1,15 @@
-const clubsReducer = (state = [], action) => {
-  console.log(action)
+const clubsReducer = (state = { data: [], pending: false }, action) => {
   switch(action.type) {
+
     case "BEGIN_CLUBS_REQUEST":
       return {
         ...state,
+        data: [...state.data],
         pending: true
       };
 
     case "ADD_CLUBS":
-      const clubs = action.clubs.map(club => {
+      const clubs = action.clubs.data.map(club => {
         return {
           id: club.id,
           name: club.attributes.name,
@@ -18,11 +19,14 @@ const clubsReducer = (state = [], action) => {
         }
       });
       return {
-        ...state.concat(clubs),
+        ...state,
+        data: state.data.concat(clubs),
         pending: false
-      }
+      };
 
     default:
       return state;
   }
 }
+
+export default clubsReducer;
