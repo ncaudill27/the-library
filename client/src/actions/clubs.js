b6cea98707a7c361638d33d70d37df6be3c9a9b9
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-11 19:23:17 -0400

    Implement proxy

--- client/src/actions/clubs.js
+++ client/src/actions/clubs.js
@@ -1,7 +1,7 @@
 const fetchClubs = () => {
   return (dispatch) => {
     dispatch({type: "BEGIN_CLUBS_REQUEST"});
-    fetch('http://localhost:3001/api/v1/clubs')
+    fetch('/api/v1/clubs')
     .then(res => res.json())
     .then(json => dispatch(addClubs(json)));
   };
