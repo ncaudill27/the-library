c6541df5a59dbf674e59c449237f5fe0139f80bd
ncaudill27 <nelson.a.caudill@gmail.com>
2020-04-11 02:17:18 -0400

    --amend

--- client/src/containers/GoogleBooksContainer.js
+++ client/src/containers/GoogleBooksContainer.js
@@ -4,7 +4,7 @@ class GoogleBooksContainer extends Component {
 
   fetchBooks() {
     const google_key = process.env.REACT_APP_GOOGLE_BOOKS_KEY
-    const isbn = "978-0-7660-6059-9".replace(/-/g, '')
+    const isbn = "9780525521143".replace(/-/g, '')
     fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${google_key}`)
     // fetch('https://www.goodreads.com/author/list/18541?format=xml&key=' + key)
     .then(resp => resp.json())
