9dc751d8c3cf3b5c6654755f8b6922903e9321f7
ncaudill27 <nelson.a.caudill@gmail.com>
2020-05-03 23:20:48 -0400

    Add fallback index for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::API
 
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
+  
   private
 
   def logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
       resources :memberships, only: [:create, :destroy]
     end
   end
+
+  get '*path', to: 'application#fallback_index_html', constraints: ->
+  (request) do
+    !request.xhr? && request.format.html?
+  end
 end
