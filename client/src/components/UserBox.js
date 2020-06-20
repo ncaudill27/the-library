b2bc93bf56c8273505d1b4fdf8f6a772d999d7c5
ncaudill27 <nelson.a.caudill@gmail.com>
2020-06-19 23:15:36 -0400

    Add logout button to UserBox

--- client/src/components/UserBox.js
+++ client/src/components/UserBox.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { logOutUser } from '../actions/users';
 import Avatar from './Avatar';
 
 const UserBox = ({user, logOutUser}) => {
+
+  const [showing, showingSet] = useState(false);
+  const show = () => showingSet(true);
+  const hide = () => showingSet(false);
+  
   return (
-    <div className='User-box'>
-      <Avatar avatar={user.avatar} showing={user.username} />
+    <div onMouseEnter={show} onMouseLeave={hide} className='User-box'>
+      { showing ? <button className='logout'>logout</button> : <Avatar avatar={user.avatar} showing={user.username} /> }
       <br/>
       <NavLink to={`/${user.username}`} exact className='Navlink'><h3>{user.username}</h3></NavLink>
   </div>
