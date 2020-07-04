037702da9039a8dcb75c593f130058afd13942d6
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-04 15:28:02 -0400

    Remove the last of old association references effectively causing breakpoints throughout the application

--- client/src/reducers/clubsReducer.js
+++ client/src/reducers/clubsReducer.js
@@ -9,7 +9,7 @@ const clubsReducer = (state = initialState, action) => {
   switch(action.type) {
 
     case "BEGIN_CLUBS_REQUEST":
-      return {...state, data: [...state.data], pending: true};
+      return { ...state, data: [...state.data], pending: true };
 
     case "ADD_CLUBS":
       clubs = action.clubs.data.map(club => {
@@ -19,44 +19,40 @@ const clubsReducer = (state = initialState, action) => {
           description: club.attributes.description,
           avatar: club.attributes.avatar,
           activeBook: "9780545010221",
-          memberIds: club.relationships.users.data.map(user => user.id),
-          threadIds: club.relationships.boards.data.map(board => board.id),
+          // memberIds: club.relationships.users.data.map(user => user.id),
+          threadIds: club.relationships.boards.data.map( board => board.id ),
         };
       });
 
-      return {...state, data: state.data.concat(clubs), pending: false};
+      return { ...state, data: state.data.concat(clubs), pending: false };
 
     case "CREATE_CLUB":
-      console.log(action);
-
       let {
         id,
         attributes: {
           name, description
-        },
-        relationships: {
-          users: { data: users}
         }
-      } = action.club.data
+      } = action.club.data;
       
       club = {
         id,
         name,
         description,
-        memberIds: users.map(user => user.id),
+        activeBook: "9780545010221",
+        // memberIds: users.map(user => user.id),
         threadIds: []
-      }
+      };
 
-      return {...state, data: state.data.concat(club), pending: false};
+      return { ...state, data: state.data.concat(club), pending: false };
 
-    case "ADD_CLUB_MEMBER":
-      club = state.data.find(c => c.id === action.clubId);
-      updatedMembers = club.memberIds.concat(action.userId);
-      updatedClub = {...club, memberIds: updatedMembers};
-      updatedClubs = state.data.map(c => c.id !== club.id ? c : updatedClub);
+    // case "ADD_CLUB_MEMBER":
+    //   club = state.data.find(c => c.id === action.clubId);
+    //   updatedMembers = club.memberIds.concat(action.userId);
+    //   updatedClub = {...club, memberIds: updatedMembers};
+    //   updatedClubs = state.data.map(c => c.id !== club.id ? c : updatedClub);
       
-      return {...state, data: updatedClubs, pending: false};
-      
+    //   return {...state, data: updatedClubs, pending: false};
+
     // case "REMOVE_CLUB_MEMBER":
     //   club = state.data.find(c => c.id === action.clubId);
     //   updatedMembers = club.memberIds.filter(id => id !== action.userId);
