353e0a1bc2377440bbdbea1ffd6b807533fc4c04
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-10 22:48:59 -0400

    Layout navbar

--- client/src/components/ClubSideBar.js
+++ client/src/components/ClubSideBar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const ClubSideBar = ({id, name, avatar}) =>
   <NavLink to={`/clubs/${id}`} exact className='Navlink' >
     <div className='Club-sidebar'>
-      <Avatar avatar={avatar} showing={name} />
+      {/* <Avatar avatar={avatar} showing={name} /> */}
       <h3>{name}</h3>
     </div>
   </NavLink>;
--- client/src/containers/SideBarContainer.js
+++ client/src/containers/SideBarContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { addClub } from '../actions/users';
@@ -6,31 +6,84 @@ import UserBox from '../components/UserBox';
 import ClubList from '../components/ClubList';
 import SideNav from '../components/SideNav';
 
-class SidebarContainer extends Component {
+import MenuIcon from '@material-ui/icons/MenuBook';
+import { Button, IconButton, AppBar, Toolbar, Typography, Menu, MenuItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-  render() {
-    const {currentUser} = this.props
-    return (
-      <div className='Sidebar'>
-        {
-          !!currentUser
-          ? <>
-            <UserBox user={currentUser} />
-            <NavLink to='/bestsellers' exact className='Create-club Navlink'
-              ><div className='Club-sidebar Create-club'
-                ><h3>Bestsellers</h3></div></NavLink>
-            <NavLink to='/clubs' exact className='Create-club Navlink'
-              ><div className='Club-sidebar Create-club'
-                ><h3>Clubs</h3></div></NavLink>
-            <ClubList styling='sidebar' />
-            </>
-          : <SideNav postion='sidebar' />
-        }
-      </div>
-    );
+const useStyles = makeStyles( theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
   }
+}));
+
+
+function SidebarContainer({currentUser}) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = e => setAnchorEl(e.currentTarget);
+  const handleClose = () => setAnchorEl(null);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon onClick={handleMenu} />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            The Library
+          </Typography>
+          <Button>Log Out</Button>
+          <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right"
+          }}
+          open={open}
+          onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <NavLink
+                to='/bestsellers'
+                exact
+                className='Create-club Navlink'
+              >
+                Bestsellers {/* <----------- link text */}
+              </NavLink>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+            <NavLink
+              to='/clubs'
+              exact
+              className='Create-club Navlink'
+            >
+              Clubs {/* <--------------- link text */}
+            </NavLink>
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
+// {
+//   !!currentUser
+//   ? <>
+//     <UserBox user={currentUser} />
+//     <ClubList styling='sidebar' />
+//     </>
+//   : <SideNav postion='sidebar' />
+// }
 const mapStateToProps = ({users}) => {
   return {
     currentUser: users.currentUser
