0654b67285dc806b7d8aab50a8c64d8e62246c31
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-11 17:58:33 -0400

    Set up CommentList to use Grow component

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,48 +1,50 @@
 import React, { Component } from 'react';
 import Comment from './Comment';
+import CommentField from './CommentField';
 import { deleteCommentRequest } from '../actions/comments';
 import { connect } from 'react-redux';
+import { Grow } from '@material-ui/core';
 
-class CommentList extends Component {
+function CommentList({ deleteCommentRequest, comments, open, threadId, handleChange, handleSubmit, currentUser, currentUserIsMod }) {
 
-  sortCommentsByCreation = () => {
-    return this.props.comments
-    .sort((c1, c2) => new Date(c2.posted) - new Date(c1.posted));
+  const sortCommentsByCreation = () => {
+    return comments.sort((c1, c2) => new Date(c2.posted) - new Date(c1.posted));
   }
   
-  renderComments() {
-    let {
-      deleteComment,
-      sortCommentsByCreation,
-      props: {
-        comments
-      }
-    } = this;
+  const renderComments = () => {
 
-    comments = sortCommentsByCreation();
+    let sortedComments = sortCommentsByCreation();
 
-    return comments.map( comment => {
-      return <Comment
-        key={comment.id}
-        {...comment}
-        deleteComment={deleteComment}
-        currentUserIsMod={this.props.currentUserIsMod}
-      />;
+    return sortedComments.map( comment => {
+      return (
+        <Grow in={open} style={{transformOrigin: '0 0 0'}} { ...(open ? { timeout: 500 } : {}) }>
+          <Comment
+            key={comment.id}
+            {...comment}
+            deleteComment={deleteCommentRequest}
+            currentUserIsMod={currentUserIsMod}
+          /> 
+        </Grow>
+      )
     });
   }
 
-  deleteComment = (e) => {
+  const deleteComment = (e) => {
     const commentId = e.target.parentNode.dataset.commentId;
     this.props.deleteCommentRequest(commentId);
   }
   
-  render() {
-    return (
-      <div className='list-comments'>
-        {this.renderComments()}
-      </div>
-    );
-  }
+  return (
+    <div className='list-comments'>
+      { renderComments() }
+      <CommentField
+        currentUser={currentUser}
+        threadId={threadId}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
 }
 
 export default connect( null, { deleteCommentRequest })(CommentList);
