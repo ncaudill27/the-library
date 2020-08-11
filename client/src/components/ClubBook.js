3403c159ebaa8083c1d06b06467de067a72aebf7
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-11 02:14:12 -0400

    Reach good point to move from ClubBook

--- client/src/components/ClubBook.js
+++ client/src/components/ClubBook.js
@@ -3,12 +3,16 @@ import StarRating from './StarRating';
 /* ----------
   Material imports
 ----------- */
-import { Grid, Container, Typography } from '@material-ui/core';
+import { Grid, Typography, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles( themes => ({
   root: {
     flexGrow: 1
+  },
+  paper: {
+    padding: themes.spacing(2),
+    backgroundColor: '#f0f0f0'
   },
   image: {
     height: 'auto',
@@ -20,35 +24,42 @@ const useStyles = makeStyles({
     maxWidth: '100%',
     maxHeight: '100%'
   }
-});
+}));
 
-function ClubBook({ title, authors, averageRating, imageLinks }) {
+function ClubBook({ title, authors, averageRating, imageLinks, description }) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={4} justify="space-around">
-        <Grid item xs={6} container direction="column" spacing={2} justify="flex-start" alignItems="flex-start">
-          <Grid item>
-            <Typography variant='h6'>
-                {title}
+      <Paper elevation={3} className={classes.paper}>
+        <Grid container spacing={1} justify="flex-start">
+          <Grid item xs={6} container direction="column" spacing={2} justify="flex-start" alignItems="flex-start">
+            <Grid item>
+              <Typography variant='h5'>
+                  {title}
+                </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='h6'>
+                Author{authors.length <= 1 || 's'}: {[...authors].join(', ')}
               </Typography>
+            </Grid>
+            <StarRating count={averageRating} />
+            <Grid item>
+              {/* <Container className={classes.image}> */}
+                <img className={classes.img} src={ imageLinks ? imageLinks.thumbnail : ''} alt={title + " Cover Art"} />
+              {/* </Container> */}
+            </Grid>
+          </Grid>
+          <Grid item xs={6} container alignItems="center" justify="flex-start">
+            <Grid item>
+                <Typography paragraph>
+                    {description}
+                </Typography>
+              </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={6} spacing={1} container direction="column" justify="flex-start" alignItems="flex-start">
-          <Grid item>
-            <Typography paragraph>
-              {[...authors].join(', ')}
-            </Typography>
-          </Grid>
-          <StarRating count={averageRating} />
-          <Grid item>
-            {/* <Container className={classes.image}> */}
-              <img className={classes.img} src={ imageLinks ? imageLinks.thumbnail : ''} alt={title + " Cover Art"} />
-            {/* </Container> */}
-          </Grid>
-        </Grid>
-      </Grid>
+      </Paper>
     </div>
   );
 }
