b009c953ddda77878436acf61624bd6e594c4dae
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-16 23:53:39 -0400

    Bring NewUser back to functionality

--- client/src/components/NewUser.js
+++ client/src/components/NewUser.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-import FormField from './FormField';
 import { connect } from 'react-redux';
 import { updateUserRequest, loginUser } from '../actions/users';
-import { Typography } from '@material-ui/core'
+import { Box, Button, Typography, FormControl, TextField, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles( themes => ({
+  form: {
+    margin: themes.spacing(3)
+  }
+}));
 
 function NewUser({currentUser, loginUser, updateUserRequest}) {
+  const classes = useStyles();
 
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
@@ -28,7 +34,7 @@ function NewUser({currentUser, loginUser, updateUserRequest}) {
   };
     
   return (
-    <div class='New-user'>
+    <Box className={classes.form}>
       <Typography variant='h4'>
         Welcome!
       </Typography> 
@@ -36,7 +42,13 @@ function NewUser({currentUser, loginUser, updateUserRequest}) {
         Please fill out this information.
         Don't worry your name or email won't be made public.
       </Typography>
-    </div>
+      <FormControl fullWidth>
+        <TextField label='Name' value={name} onChange={ e => handleChange(setName, e) } />
+        <TextField label='Username' value={username} onChange={ e => handleChange(setUsername, e) } />
+        <TextField label='Bio' value={bio} multiline rowsMax={6} onChange={ e => handleChange(setBio, e) } />
+        <Button onClick={handleSubmit}>Enter Library</Button>
+      </FormControl>
+    </Box>
   );
 }
 
