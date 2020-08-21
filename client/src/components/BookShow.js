5dea578e812a97ece88e7cdb1ba66591ce14350e
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-21 02:45:02 -0400

    apply background colors to library paper

--- client/src/components/BookShow.js
+++ client/src/components/BookShow.js
@@ -3,23 +3,27 @@ import StarRating from './StarRating';
 /* ----------
   Material imports
 ----------- */
-import { Grid, Typography, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography, Paper, Box } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 const { REACT_APP_GOOGLE_BOOKS_KEY } = process.env;
 
-const useStyles = makeStyles( themes => ({
+const useStyles = makeStyles( theme => ({
   root: {
     flexGrow: 1
   },
   paper: {
-    padding: themes.spacing(2),
-    marginBottom: themes.spacing(4),
-    backgroundColor: '#f0f0f0',
+    padding: theme.spacing(1),
+    marginBottom: theme.spacing(4),
+    backgroundColor: '#eaa29f',
     overflow: 'auto'
   },
+  title: {
+    height: '100%',
+    width: '100%'
+  },
   details: {
-    margin: themes.spacing(1),
-    padding: themes.spacing(1),
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
     float: 'right',
   },
   image: {
@@ -31,13 +35,18 @@ const useStyles = makeStyles( themes => ({
     display: 'block',
     maxWidth: '100%',
     maxHeight: '100%'
+  },
+  description: {
+    marginTop: theme.spacing(2)
   }
 }));
 
 function BookShow({ isbn, hide }) {
   const classes = useStyles();
+  const theme = useTheme();
 
   const [book, setBook] = useState(null);
+  console.log(theme);
 
   useEffect( () => {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${REACT_APP_GOOGLE_BOOKS_KEY}`)
@@ -48,13 +57,15 @@ function BookShow({ isbn, hide }) {
 
   if (!book) return <h2 onClick={hide}>loading...</h2>;
   else return (
-    <Paper elevation={3} className={classes.paper} onClick={hide}>
+    <Paper elevation={3} className={classes.paper} onClick={hide} color={theme.palette.secondary.light}>
+      <Grid item >
+        <Box className={classes.title}>
+        <Typography variant='h5' align='center'>
+            {book.title}
+        </Typography>
+        </Box>
+      </Grid>
       <Grid className={classes.details} xs={6} item container direction='column' spacing={0} justify='flex-start' alignItems='center'>
-        <Grid item>
-          <Typography variant='h5' align='center'>
-              {book.title}
-            </Typography>
-        </Grid>
         <Grid item>
           <Typography variant='h6' align='center'>
             Author{book.authors.length <= 1 || 's'}: {[...book.authors].join(', ')}
@@ -67,7 +78,7 @@ function BookShow({ isbn, hide }) {
           <img className={classes.img} src={book.imageLinks ? book.imageLinks.thumbnail : ''} alt={book.title + " Cover Art"} />
         </Grid>
       </Grid>
-      <Typography paragraph>
+      <Typography paragraph className={classes.description}>
         {book.description}
       </Typography>
     </Paper>
