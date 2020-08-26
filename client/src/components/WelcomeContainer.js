e9dabed353ed0c2ad71708a5512126fc70d3218d
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-26 13:49:52 -0400

    Restyle WelcomeContainer message

--- client/src/components/WelcomeContainer.js
+++ client/src/components/WelcomeContainer.js
@@ -18,7 +18,12 @@ const useStyles = makeStyles( theme => ({
     backgroundColor: '#f0f0f0',
   },
   soon: {
-    marginBottom: theme.spacing(2)
+    padding: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main
+  },
+  paper: {
+    backgroundColor: '#f0f0f0',
+    padding: theme.spacing(1)
   }
 }))
 
@@ -65,31 +70,33 @@ function SelectBookMessage({classes}) {
 
 function ComingSoon({classes}) {
 
-  return (
-    <>
-      <Typography variant='h3' className={classes.soon}>
-        Coming Soon!
-      </Typography>
-      <Typography variant='h6'>
-        Recommendations
-      </Typography>
-      <Typography variant='subtitle1' paragraph>
-        Have the hottest/latest books and clubs brought directly to you!
-      </Typography>
-      <Typography variant='h6'>
-        Search
-      </Typography>
-      <Typography variant='subtitle1' paragraph>
-        The current selection of NY Times bestsellers not enough for your needs? Soon you will have the full power of Google available!
-      </Typography>
-      <Typography variant='h6'>
-        Polling
-      </Typography>
-      <Typography variant='subtitle1' paragraph>
-        Club mods, an easier way to vote on your clubs next book will be coming shortly! Allowing users to vote for their next read.
-      </Typography>
-    </>
-  );
+  return (<>
+    <Paper className={classes.soon}>
+          <Typography variant='h3'>
+            Coming Soon!
+          </Typography>
+        </Paper>
+        <Paper className={classes.paper} square>
+        <Typography variant='h6'>
+          Recommendations
+        </Typography>
+        <Typography variant='subtitle1' paragraph>
+          Have the hottest/latest books and clubs brought directly to you!
+        </Typography>
+        <Typography variant='h6'>
+          Search
+        </Typography>
+        <Typography variant='subtitle1' paragraph>
+          The current selection of NY Times bestsellers not enough for your needs? Soon you will have the full power of Google available!
+        </Typography>
+        <Typography variant='h6'>
+          Polling
+        </Typography>
+        <Typography variant='subtitle1' paragraph>
+          Club mods, an easier way to vote on your clubs next book will be coming shortly! Allowing users to vote for their next read.
+        </Typography>
+      </Paper>  
+  </>);
 }
 
 function OnboardingMessage({classes, isOnboarded, isMember, isReading}) {
